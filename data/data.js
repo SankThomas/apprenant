155e1ef2ecbe82510d0c80edf45caf9c45d23a70
Thomas Sankara <thomassankara74@gmail.com>
2021-03-01 21:17:36 +0300

    saluation page data

---
+++ data/data.js
@@ -0,0 +1,44 @@
+const items = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "Bonjour! - Hello!",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Salut! - Hi!",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Coucou! - Hello!",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d73",
+    title: "Au revoir! – Goodbye!",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d74",
+    title: "Bonne nuit! – Good night!",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d75",
+    title: "Merci! - Thank you!",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d76",
+    title: "De rien! - not all!",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d77",
+    title: "Comment ça va? – How are you?",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d78",
+    title: "Ça va bien merci- Am fine thank you.",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d79",
+    title: "Ça va mal – I am not okay",
+  },
+]
+
+export default items
