64eb52f839636a950b60c43b99eda91168f7e623
Thomas Sankara <thomassankara74@gmail.com>
2021-03-01 21:17:49 +0300

    numbers page data

---
+++ data/numbers.js
@@ -0,0 +1,52 @@
+export const numbers = [
+  {
+    id: 1,
+    title: "One",
+    translation: "Un",
+  },
+  {
+    id: 2,
+    title: "Two",
+    translation: "Deux",
+  },
+  {
+    id: 3,
+    title: "Three",
+    translation: "Trois",
+  },
+  {
+    id: 4,
+    title: "Four",
+    translation: "Quatre",
+  },
+  {
+    id: 5,
+    title: "Five",
+    translation: "Cinq",
+  },
+  {
+    id: 6,
+    title: "Six",
+    translation: "Six",
+  },
+  {
+    id: 7,
+    title: "Seven",
+    translation: "Sept",
+  },
+  {
+    id: 8,
+    title: "Eight",
+    translation: "Huit",
+  },
+  {
+    id: 9,
+    title: "Nine",
+    translation: "Neuf",
+  },
+  {
+    id: 10,
+    title: "Ten",
+    translation: "Dix",
+  },
+]
