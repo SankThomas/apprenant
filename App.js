35dca63edaf6ed0ebafe905982828b3699b55b68
Thomas Sankara <thomassankara74@gmail.com>
2021-03-01 21:19:49 +0300

    routing and render app

--- App.js
+++ App.js
@@ -1,21 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler"
+import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import Ionicons from "react-native-vector-icons/Ionicons"
+import { StyleSheet } from "react-native"
+import HomeScreen from "./screens/HomeScreen"
+import About from "./screens/About"
+import Salutation from "./screens/Salutation"
+import Lessons from "./screens/Lessons"
+import Numbers from "./screens/NumbersLesson"
 
-export default function App() {
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Apprenant"
+          component={HomeScreen}
+          options={{
+            title: "Apprenant",
+            headerStyle: {
+              backgroundColor: "#059669",
+            },
+            headerTintColor: "#fff",
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="About"
+          component={About}
+          options={{
+            headerStyle: {
+              backgroundColor: "#059669",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Stack.Screen
+          name="Salutation"
+          component={Salutation}
+          options={{
+            headerStyle: {
+              backgroundColor: "#059669",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Stack.Screen
+          name="Lessons"
+          component={Lessons}
+          options={{
+            headerStyle: {
+              backgroundColor: "#059669",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+        <Stack.Screen
+          name="Numbers"
+          component={Numbers}
+          options={{
+            headerStyle: {
+              backgroundColor: "#059669",
+            },
+            headerTintColor: "#fff",
+          }}
+        />
+      </Stack.Navigator>
+      {/* <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName
+
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline"
+            } else if (route.name === "About") {
+              iconName = focused ? "ios-list-box" : "ios-list"
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="About" component={About} />
+      </Tab.Navigator> */}
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
